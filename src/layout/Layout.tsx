d39e792e20b80bac3ff1d9ed147d6c52c0021908
rohtelah10 <rohtelah10@gmail.com>
2025-12-10 01:26:39 +0530

    responsiveness improved

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,34 +1,37 @@
-import styled from 'styled-components';
-import SplitImg from '../assets/images/logo.svg';
-import TipCalculator from '../features/TipCalculator';
+import styled from "styled-components";
+import SplitImg from "../assets/images/logo.svg";
+import TipCalculator from "../features/TipCalculator";
 
 const Layout = () => {
   return (
     <Wrapper>
-        <SplitterImg src={SplitImg} alt=""></SplitterImg>
-        <TipCalculator></TipCalculator>
+      <SplitterImg src={SplitImg} alt=""></SplitterImg>
+      <TipCalculator></TipCalculator>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
 
 const Wrapper = styled.div`
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 88px;
-    background-color: ${ ({theme}) => theme.colors["grey-200"]};
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 88px;
+  background-color: ${({ theme }) => theme.colors["grey-200"]};
 
-    @media (min-width: 650px) and (max-width: 1024px) {
-      padding-bottom: 80px; 
-    }
+  @media (min-width: 650px) and (max-width: 1024px) {
+    padding-bottom: 80px;
+  }
 `;
 
 const SplitterImg = styled.img`
-    width: 87px;
-    height: 53px;
+  width: 87px;
+  height: 53px;
+
+  @media (max-width: 650px) {
     margin-top: 50px;
+  }
 `;
